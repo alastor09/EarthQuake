d6ba7274b8ecce06a80ade3e0b8256473283c534
Soan Saini <soansaini@soans-imac.gateway>
2017-02-13 19:44:01 +1100

    - Clean up

--- EarthQuake/EarthQuake/NetworkLayer/FetchEarthQuakes/GetEarthQuakesAPIClient.swift
+++ EarthQuake/EarthQuake/NetworkLayer/FetchEarthQuakes/GetEarthQuakesAPIClient.swift
@@ -42,38 +42,3 @@ class GetEarthQuakesAPIClient: GetBaseAPIClient {
         return arrayOfEarthQuakeModels
     }
 }
-
-
-/*
- 
- 
- var iso8601: String { return Formatter.earthQuakeServerDateFormatter.string(from: self) }
-
- 
- {
- "count": "21740",
- "earthquakes": [
- {
- "src": "us",
- "eqid": "c000is61",
- "timedate": "2013-07-29 22:22:48",
- "lat": "7.6413",
- "lon": "93.6871",
- "magnitude": "4.6",
- "depth": "40.90",
- "region": "Nicobar Islands, India region"
- },
- {
- "src": "us",
- "eqid": "c000is4s",
- "timedate": "2013-07-29 21:52:12",
- "lat": "-57.7816",
- "lon": "-25.3260",
- "magnitude": "5.2",
- "depth": "53.50",
- "region": "South Sandwich Islands region"
- }
- ]
- }
- 
- */
